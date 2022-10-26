2a2a1b435823ffe2d2fb6a4a7725bc16d9341899
unknown <abdullahalmohidofficial@gmail.com>
2022-10-26 16:20:52 +0600

    first commit

--- medilab-react/src/App.js
+++ medilab-react/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div>
-
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non adipisci quaerat deleniti similique corrupti, perferendis laboriosam inventore, voluptates et modi hic officia voluptas harum atque assumenda vero! Fuga, laborum reiciendis!</p>
     </div>
   );
 }
