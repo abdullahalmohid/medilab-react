4281f1c6e581a6115684f0a5e49ca8caa93693b2
unknown <abdullahalmohidofficial@gmail.com>
2022-10-27 16:26:36 +0600

    header done

--- medilab-react/src/App.js
+++ medilab-react/src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Header from './Component/Header/Header';
 
 function App() {
   return (
     <div>
-<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non adipisci quaerat deleniti similique corrupti, perferendis laboriosam inventore, voluptates et modi hic officia voluptas harum atque assumenda vero! Fuga, laborum reiciendis!</p>
+<Header></Header>
     </div>
   );
 }
